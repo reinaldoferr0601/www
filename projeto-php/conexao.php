81b3b8a2f42d7a2e5490ff891e5fec0598cfd849
reinaldoferreira <reinaldoferreira@outlook.com>
2018-08-07 21:41:42 -0300

    commit

--- projeto-php/conexao.php
+++ projeto-php/conexao.php
@@ -12,6 +12,4 @@ $conexao = mysqli_connect($servername,$username,$password,$database);
 if(!$conexao) {
 	die("Connection failed: " .mysqli_connect_error());
 } 
-echo "Conected successfully";
-
 ?>
